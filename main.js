1862b4249abc8b83678b19ae53e968dc472b15a7
Fran <89055928+Fran26TE@users.noreply.github.com>
2021-08-24 05:42:29 +0200

    modificaciones generales

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 const tema1 = document.querySelector('#tema1');
 const tema2 = document.querySelector('#tema2');
-const color1 = document.querySelector('#color1');
-const color2 = document.querySelector('#color2');
-const color3 = document.querySelector('#color3');
+const colores1 = document.querySelector('#colores1');
+const colores2 = document.querySelector('#colores2');
+const colores3 = document.querySelector('#colores3');
 
 
 tema1.addEventListener('click', e => {
-    var elemento = document.getElementById("calculadora");
-    elemento.className = "calculadora";
+    var tema = document.getElementById("forma");
+    tema.className = "forma1";
 });
 
 tema2.addEventListener('click', e => {
-    var elemento = document.getElementById("calculadora");
-    elemento.className = "tema2";
+    var tema = document.getElementById("forma");
+    tema.className = "forma2";
+    
 });
 
+colores1.addEventListener('click', e => {
+    var color = document.getElementById("colores");
+    color.className = "color1";
+});
 
+colores2.addEventListener('click', e => {
+    var color = document.getElementById("colores");
+    color.className = "color2";
+});
+
+colores3.addEventListener('click', e => {
+    var color = document.getElementById("colores");
+    color.className = "color3";
+});
