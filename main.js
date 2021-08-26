b98bef934d2a5f834bdc7f8658eb7c75dbaa0e3f
Fran <89055928+Fran26TE@users.noreply.github.com>
2021-08-26 19:32:01 +0200

    actualización

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
+
 const skin = document.querySelector('#skin');
 const apariencia = document.querySelector('#calculadora');
-
 
 load();
 
 skin.addEventListener('click', e => {
-	calculadora.classList.toggle('aspecto');
-    store(body.classList.contains('aspecto'));
+    body.classList.toggle('aspecto');
+    calculadora.classList.toggle('aspecto');
 
+    store(body.classList.contains('aspecto'));
 });
 
 function load() {
-	const aspecto = localStorage.getItem('aspecto');
+    const aspecto = localStorage.getItem('aspecto');
 
-	if (!aspecto) {
-		store('false');
-	} else if (aspecto == 'true') {
-		apariencia.classList.add('aspecto');
-	}
+    if (!aspecto) {
+        store('aspecto');
+    } else if (aspecto == 'true') {
+        body.classList.add('aspecto');
+        apariencia.classList.add('aspecto');
+    }
 }
 
 function store(value) {
-	localStorage.setItem('aspecto', value);
+    localStorage.setItem('aspecto', value);
 }
+
 
 
